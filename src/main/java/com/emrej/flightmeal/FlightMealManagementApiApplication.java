f51ded8f3c314a9ccbca3c7fedc32eed0d4f89cb
Emre Ceylan <ceylanemre@gmail.com>
2019-09-23 20:17:13 +0200

    Project setup

---
+++ src/main/java/com/emrej/flightmeal/FlightMealManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.emrej.flightmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMealManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightMealManagementApiApplication.class, args);
+    }
+
+}
